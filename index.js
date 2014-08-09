93ff53a7b01caef191cefdd1ac84420428fe4a61
Rinat <nutsgamedev@gmail.com>
2014-08-09 21:09:56 +0600

    refactor

--- index.js
+++ index.js
@@ -20,6 +20,4 @@ string.format = function( /*..*/ ) {
     return result;
 }
 
-
-var test = string.format('Hello {0}, My name is {1}', 'World', 'Rinat');
-console.log(test);
+module.exports = string;
