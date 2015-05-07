09490644c451a33bcd4855adceb441439b3a918f
Rinat <nutsgamedev@gmail.com>
2015-05-07 20:26:58 +0500

    Add realization on the Typescript

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-string-format Version: 0.2.1.
+string-format Version: 0.2.2.
 JSHint passed.
 =============
+
+Add realization on the Typescript [typescript](https://github.com/RinatMullayanov/string-format/tree/master/typescript)
 
 Lib for work with string like String.Format from C#
 http://msdn.microsoft.com/en-us/library/System.String.Format(v=vs.110).aspx
---
+++ typescript/index.ts
@@ -0,0 +1,35 @@
+/// <reference path="../typings/tsd.d.ts"/>
+var string = <any> {};
+/*
+    Replaces the format item in a specified string
+    with the string representation of a corresponding object in a specified array.
+*/
+string.format = function format(...params) {
+  var allArgs = params;
+  var format = allArgs[0];
+  var result = format.substring(0, format.length);
+
+  if (allArgs.length === 2 && typeof allArgs[1] === 'object') {
+      // object pass like two argument
+      var obj = allArgs[1];
+      for (var prop in obj) {
+          if (obj.hasOwnProperty(prop)) {
+              result = result.replace('{' + prop + '}', obj[prop]);
+          }
+      }
+  } else {
+      // many args separate elements
+      var args = allArgs.filter(function(item, i) {
+          return i > 0;
+      });
+
+      for (var i = 0, len = args.length; i < len; i++) {
+          result = result.replace('{' + i + '}', args[i]);
+      }
+  }
+
+  return result;
+
+}
+
+export = string;
